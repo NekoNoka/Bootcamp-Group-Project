7c2d4e631fadcdaa66162b588619a96b7f33bc8a
saanspeur <evelin.ortega018@gmail.com>
2023-05-02 21:13:39 -0700

    added page buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,20 @@ let nytimesImgContainer = document.querySelector('#secondA .content');
 let searchBar = document.querySelector('.search_bar input');
 let history = localStorage.getItem("omdbStorage") ? JSON.parse(localStorage.getItem("omdbStorage")) : [];
 let history2 = localStorage.getItem("nytimesStorage") ? JSON.parse(localStorage.getItem("nytimesStorage")) : [];
+var omdbPageNumber = 1;
+var previousSearch = "";
 
-function getApi() {
-   if (!searchBar.value) return;
+function getApi(newQuery) {
+   if (newQuery) previousSearch = searchBar.value;
+   if (!previousSearch) return;
+   if (newQuery) omdbPageNumber = 1;
    // eeff1550
    // GrJLtpKFDzJKSy1um4IkiszoYQGrxb26
-   let omdb = "https://www.omdbapi.com/?apikey=eeff1550&type=movie" + "&s=" + searchBar.value;
-   let nytimes = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=GrJLtpKFDzJKSy1um4IkiszoYQGrxb26" + "&query=" + searchBar.value;
+   let omdb = "https://www.omdbapi.com/?apikey=eeff1550&type=movie" + "&page=" + omdbPageNumber + "&s=" + previousSearch;
+   let nytimes = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=GrJLtpKFDzJKSy1um4IkiszoYQGrxb26" + "&query=" + previousSearch;
    (function () {
       for (let i = 0; i < history.length; i++) {
-         if (history[i].name === searchBar.value) {
+         if (history[i].name === previousSearch && omdbPageNumber == history[i].page) {
             omdbImgContainer.innerHTML = "";
             let data = history[i].value;
             for (let i = 0; i < data.length; i++) {
@@ -29,17 +33,17 @@ function getApi() {
       fetch(omdb).then(function (response) {
          response.json().then(function (json) {
             if (json.Response !== "True") return console.error("The search returned no results for omdb"); // we probably want to do more than logging an error.
-            history.push({ value: json.Search, name: searchBar.value });
+            history.push({ value: json.Search, name: previousSearch, page: omdbPageNumber });
             localStorage.setItem("omdbStorage", JSON.stringify(history));
             omdbImgContainer.innerHTML = "";
             let data = json.Search;
             for (let i = 0; i < data.length; i++) {
-  
-                  var title = json['Title']
-                  var time = json['Year']
-      
-                  movie.innerHTML = `Movie: ${title}`
-                  Year.innerHTML = `Year: ${time} `
+
+               var title = json['Title']
+               var time = json['Year']
+
+               // movie.innerHTML = `Movie: ${title}`
+               // Year.innerHTML = `Year: ${time} `
 
                let movie = data[i];
                let img = document.createElement('img');
@@ -51,42 +55,71 @@ function getApi() {
          });
       });
    })();
-   (function () {
-      for (let i = 0; i < history2.length; i++) {
-         if (history2[i].name === searchBar.value) {
-            nytimesImgContainer.innerHTML = "";
-            let data = history2[i].value.results;
-            for (let i = 0; i < data.length; i++) {
-               let movie = data[i];
-               let img = document.createElement('img');
-               if (movie.multimedia === null) continue;
-               img.classList.add("poster");
-               img.src = movie.multimedia.src;
-               nytimesImgContainer.appendChild(img);
+   if (newQuery) {
+      (function () {
+         for (let i = 0; i < history2.length; i++) {
+            if (history2[i].name === searchBar.value) {
+               nytimesImgContainer.innerHTML = "";
+               let data = history2[i].value.results;
+               for (let i = 0; i < data.length; i++) {
+                  let movie = data[i];
+                  let img = document.createElement('img');
+                  if (movie.multimedia === null) continue;
+                  img.classList.add("poster");
+                  img.src = movie.multimedia.src;
+                  nytimesImgContainer.appendChild(img);
+               }
+               return;
             }
-            return;
          }
-      }
-      fetch(nytimes).then(function (response) {
-         console.log(response);
-         response.json().then(function (json) {
-            console.log(json.status, json);
-            if (json.status !== "OK") return console.error("The search broke for nytimes"); // we probably want to do more than logging an error.
-            if (json.results === null) return console.error("The search returned no results for nytimes"); // we probably want to do more than logging an error.
-            history2.push({ value: json, name: searchBar.value });
-            localStorage.setItem("nytimesStorage", JSON.stringify(history2));
-            nytimesImgContainer.innerHTML = "";
-            let data = json.results;
-            console.log(data);
-            for (let i = 0; i < data.length; i++) {
-               let movie = data[i];
-               let img = document.createElement('img');
-               if (movie.multimedia === null) continue;
-               img.classList.add("poster");
-               img.src = movie.multimedia.src;
-               nytimesImgContainer.appendChild(img);
-            }
+         fetch(nytimes).then(function (response) {
+            console.log(response);
+            response.json().then(function (json) {
+               console.log(json.status, json);
+               if (json.status !== "OK") return console.error("The search broke for nytimes"); // we probably want to do more than logging an error.
+               if (json.results === null) return console.error("The search returned no results for nytimes"); // we probably want to do more than logging an error.
+               history2.push({ value: json, name: searchBar.value });
+               localStorage.setItem("nytimesStorage", JSON.stringify(history2));
+               nytimesImgContainer.innerHTML = "";
+               let data = json.results;
+               console.log(data);
+               for (let i = 0; i < data.length; i++) {
+                  let movie = data[i];
+                  let img = document.createElement('img');
+                  if (movie.multimedia === null) continue;
+                  img.classList.add("poster");
+                  img.src = movie.multimedia.src;
+                  nytimesImgContainer.appendChild(img);
+               }
+            });
          });
-      });
-   })();
+      })();
+   }
 }
+function setPage(id) {
+   if (id == 1) {
+      omdbPageNumber = 1
+      getApi();
+   }
+   if (id == 2) {
+      omdbPageNumber = 2
+      getApi();
+   }
+   if (id == 3) {
+      omdbPageNumber = 3
+      getApi();
+   }
+   if (id == 4) {
+      omdbPageNumber = 4
+      getApi();
+   }
+   if (id == 5) {
+      omdbPageNumber = 5
+      getApi();
+   }
+}
+
+
+
+
+
